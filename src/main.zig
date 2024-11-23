cd148b074316a3ac7217dc7f0f5e63cf3d18f2c3
Samuel <cloutiersamuel23@gmail.com>
2024-11-22 20:47:09 -0500

    Minimum functionality

--- src/main.zig
+++ src/main.zig
@@ -6,14 +6,22 @@ const testing = std.testing;
 const Instruction = @import("Instruction.zig").Instruction;
 const Token = @import("Token.zig").Token;
 
-const ParseError = error{ FileTooBig, UnclosedBrackets };
+const ParseError = error{
+    FileTooBig,
+    UnmatchedBrackets,
+};
+const RuntimeError = error{ StackPointerOutOfBounds, IllegalInstruction, OutputError };
 
 const Program = struct {
     instructions: [max_instructions]Token,
     n_instructions: u32 = 0,
+    stack: [stack_size]u8 = undefined,
+    data_ptr: usize = 0,
+    instr_ptr: usize = 0,
 
     const Self = @This();
-    const max_instructions: usize = 1024;
+    const max_instructions = 1024;
+    const stack_size = 100;
 
     pub fn addInstruction(self: *Self, instruction: Token) !void {
         if (self.n_instructions >= max_instructions) return ParseError.FileTooBig;
@@ -22,19 +30,93 @@ const Program = struct {
         self.n_instructions += 1;
     }
 
-    pub fn dumpProgram(self: Self) !void {
+    pub fn dumpTokens(self: Self) !void {
         for (0..self.n_instructions) |i| {
             try std.io.getStdOut().writer().print("{any}\n", .{self.instructions[i]});
+        }
+    }
+
+    pub fn dumpStack(self: Self) !void {
+        const stdout = std.io.getStdOut().writer();
+
+        try stdout.print("Program dump\ndata_ptr: {d}\ninstr_ptr: {d}\nstack: ", .{ self.data_ptr, self.instr_ptr });
+        for (0..stack_size) |i| {
+            try stdout.print("{d}", .{self.stack[i]});
+            if (i != stack_size - 1) {
+                try stdout.writeAll(" ");
+            }
+        }
+        try stdout.writeAll("\n");
+    }
+
+    pub fn start(self: *Self) !void {
+        @memset(&self.stack, 0);
+
+        interpret: while (self.instr_ptr < self.n_instructions) {
+            //std.debug.print("evaluating: {d}\n", .{self.instr_ptr});
+
+            switch (self.instructions[self.instr_ptr].instruction) {
+                Instruction.inc => self.stack[self.data_ptr] +%= 1,
+                Instruction.dec => self.stack[self.data_ptr] -%= 1,
+                Instruction.inc_data_p => {
+                    if (self.data_ptr == stack_size - 1) return RuntimeError.StackPointerOutOfBounds;
+                    self.data_ptr += 1;
+                },
+                Instruction.dec_data_p => {
+                    if (self.data_ptr == 0) return RuntimeError.StackPointerOutOfBounds;
+                    self.data_ptr -= 1;
+                },
+                Instruction.open_loop => {
+                    // Jump to maching bracket
+                    if (self.stack[self.data_ptr] == 0) {
+                        self.instr_ptr = self.instructions[self.instr_ptr].matching_bracket.? + 1;
+                        continue :interpret;
+                    }
+                },
+                Instruction.close_loop => {
+                    // Jump to maching bracket
+                    if (self.stack[self.data_ptr] != 0) {
+                        self.instr_ptr = self.instructions[self.instr_ptr].matching_bracket.? + 1;
+                        continue :interpret;
+                    }
+                },
+                Instruction.output => {
+                    const stdout = std.io.getStdOut().writer();
+                    const isAscii: bool = blk: {
+                        if (self.stack[self.data_ptr] < 128 and self.stack[self.data_ptr] >= 0) break :blk true;
+                        break :blk false;
+                    };
+
+                    if (isAscii) {
+                        const byte: u8 = @intCast(self.stack[self.data_ptr]);
+                        stdout.writeByte(byte) catch return RuntimeError.OutputError;
+                    }
+                },
+                Instruction.input => {
+                    const stdin = std.io.getStdIn().reader();
+
+                    const read_byte = stdin.readByte() catch 0;
+                    self.stack[self.data_ptr] = read_byte;
+                },
+                else => return RuntimeError.IllegalInstruction,
+            }
+            self.instr_ptr += 1;
         }
     }
 };
 
 pub fn main() !void {
-    var ins = std.ArrayList(Token).init(std.heap.page_allocator);
+    const txt = ">,>+++++++++,>+++++++++++[<++++++<++++++<+>>>-]<<.>.<<-.>.>.<<.";
 
-    try ins.append(Token{ .instruction = Instruction.open_loop, .pos = 0 });
+    var p = try parseProgram(txt);
 
-    std.debug.print("{any}\n", .{ins.items[0]});
+    p.start() catch |e| {
+        std.debug.print("{any}", .{e});
+        try p.dumpStack();
+        return;
+    };
+
+    try p.dumpStack();
 }
 
 fn parseProgram(data: []const u8) !Program {
@@ -47,19 +129,21 @@ fn parseProgram(data: []const u8) !Program {
         const instr = Instruction.instructionFromChar(data[i]);
         if (instr != Instruction.invalid) {
             try program.addInstruction(Token{ .instruction = instr, .pos = program.n_instructions });
-        
+
             // Find matching brackets
             if (instr == Instruction.open_loop) {
-                try brackets.append(program.n_instructions - 1); // -1 because n_instructions is incremented automatically
+                try brackets.append(program.n_instructions - 1); // -1 because n_instructions is incremented with addInstuction()
             } else if (instr == Instruction.close_loop) {
+                if (brackets.items.len == 0) return ParseError.UnmatchedBrackets;
+
                 const matching: usize = brackets.pop();
-                program.instructions[matching].matching_bracket = program.n_instructions - 1;   // update open bracket
-                program.instructions[program.n_instructions - 1].matching_bracket = matching;   // update closed (current)
+                program.instructions[matching].matching_bracket = program.n_instructions - 1; // update open bracket
+                program.instructions[program.n_instructions - 1].matching_bracket = matching; // update closed (current)
             }
         }
     }
 
-    if (brackets.items.len != 0) return ParseError.UnclosedBrackets;
+    if (brackets.items.len != 0) return ParseError.UnmatchedBrackets;
 
     return program;
 }
@@ -79,10 +163,55 @@ test "matching brackets" {
 
     const program = try parseProgram(test_ops);
 
-    try program.dumpProgram();
-
     try testing.expect(program.instructions[0].matching_bracket == 5);
     try testing.expect(program.instructions[5].matching_bracket == 0);
+}
+
+test "inc instruction" {
+    const test_ops = "+++";
+
+    var p = try parseProgram(test_ops);
+
+    try p.start();
+
+    try testing.expect(p.n_instructions == 3);
+    try testing.expect(p.stack[0] == 3);
+}
+
+test "dec instruction" {
+    const test_ops = "-";
+
+    var p = try parseProgram(test_ops);
+
+    try p.start();
+
+    try testing.expect(p.n_instructions == 1);
+    try testing.expect(p.stack[0] == 255);
+}
+
+test "move data ptr" {
+    const test_ops = "++>++-<+";
+
+    var p = try parseProgram(test_ops);
+
+    try p.start();
+
+    try testing.expect(p.stack[0] == 3);
+    try testing.expect(p.stack[1] == 1);
+}
+
+test "mov data ptr out of bounds" {
+    const test_ops = "<";
+
+    var p = try parseProgram(test_ops);
+
+    var err: RuntimeError = undefined;
+
+    p.start() catch |e| {
+        err = e;
+    };
+
+    try testing.expect(err == RuntimeError.StackPointerOutOfBounds);
 }
 
 test "parse program" {
@@ -105,4 +234,15 @@ test "parse program" {
         try testing.expect(program.instructions[i].instruction == expected.instructions[i].instruction);
         try testing.expect(program.instructions[i].pos == expected.instructions[i].pos);
     }
+}
+
+test "add two cells (5+2)" {
+    const test_ops = "++ > +++++ [<+>-]";
+
+    var p = try parseProgram(test_ops);
+
+    try p.start();
+
+    try testing.expect(p.instructions[8].matching_bracket.? == 13);
+    try testing.expect(p.stack[0] == 7);
 }
